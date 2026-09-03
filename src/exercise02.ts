0e73aa4ddca22a10e5efda0df4049d085ddbae05
flaxmartin <flaxmartin@gmail.com>
2026-09-02 21:31:08 -0400

    Update exercise02.ts

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,28 @@
+export class InvalidDNAError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "InvalidDNAError";
+  }
+}
+
 export function transcribeDNA(dna: string): string {
-  return '';
+  return dna
+    .split("")
+    .map((nucleotide) => {
+      switch (nucleotide) {
+        case "A":
+          return "U";
+        case "T":
+          return "A";
+        case "C":
+          return "G";
+        case "G":
+          return "C";
+        default:
+          throw new InvalidDNAError(
+            `Invalid DNA nucleotide: ${nucleotide}`,
+          );
+      }
+    })
+    .join("");
 }

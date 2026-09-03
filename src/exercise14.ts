62dc8e6573807b4336aef36ea3eef517c788e2b5
flaxmartin <flaxmartin@gmail.com>
2026-09-02 22:02:42 -0400

    Update exercise14.ts

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,23 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
-export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+export async function fetchPostBatch(
+  postIds: number[],
+): Promise<PostItem[]> {
+  const requests = postIds.map(async (id) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch post ${id}`);
+    }
+
+    return (await response.json()) as PostItem;
+  });
+
+  return await Promise.all(requests);
 }

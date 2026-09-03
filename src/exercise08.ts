1be77c6bf82e2f5a433197fb3cb0d8b0034f4ace
flaxmartin <flaxmartin@gmail.com>
2026-09-02 21:37:07 -0400

    Update exercise08.ts

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,31 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private handlers: {
+    [K in keyof T]?: Array<(data: T[K]) => void>;
+  } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    const eventHandlers = this.handlers[eventName];
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+    if (eventHandlers) {
+      eventHandlers.push(callback);
+    } else {
+      this.handlers[eventName] = [callback];
+    }
+  }
+
+  public emit<K extends keyof T>(
+    eventName: K,
+    data: T[K],
+  ): void {
+    const eventHandlers = this.handlers[eventName];
+
+    if (eventHandlers) {
+      eventHandlers.forEach((callback) => callback(data));
+    }
+  }
 }

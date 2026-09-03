90e24d941b2164f78f571f40353ac0d5f46ae98f
flaxmartin <flaxmartin@gmail.com>
2026-09-02 21:55:21 -0400

    Update exercise11.ts

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,12 @@
+import * as fs from "node:fs/promises";
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timestamp = new Date().toISOString();
+
+  const message = `${timestamp} - ${statusMessage}`;
+
+  await fs.writeFile(filePath, message, "utf-8");
+}
